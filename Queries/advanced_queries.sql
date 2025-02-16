41b29cf082a2ba2dd759ec9fdfe653f2caa1a064
mgclaflin <mgclaflin@crimson.ua.edu>
2025-02-16 12:04:23 +0100

    updated with new queries

--- Queries/advanced_queries.sql
+++ Queries/advanced_queries.sql
@@ -2,3 +2,9 @@
 select row_number() over (order by salary desc) as row_num, employee_id, first_name, last_name, j.job_title, salary
 from employees join jobs j on employees.job_id=j.job_id;
 
+-- write a query to rank employees based on their salary within their deparent_id
+select employee_id, first_name, last_name, department_id, salary, RANK() over (partition by department_id order by salary desc) as rank_in_department
+from employees
+order by department_id, rank_in_department;
+
+--Write a query to display the salary from each employee and the salary of the previous employee in the same department, using the LAG() function
